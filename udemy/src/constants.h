804236cf5227bc07d6026a5267708309e4342b1f
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-01-21 11:29:25 +0900

    [Add] main.c .h to udemy dir

---
+++ udemy/src/constants.h
@@ -0,0 +1,18 @@
+#define TRUE	0
+#define FALSE	1
+
+#define WIN_WIDTH	800
+#define WIN_HEIGHT	600
+
+#define X_EVENT_KEY_PRESS	2
+#define X_EVENT_KEY_release	3
+#define X_EVENT_KEY_EXIT	17
+
+#define KEY_ESC	53
+
+typedef struct	s_param {
+	int		x;
+	int		y;
+	char	str[3];
+}				t_param;
+
--- udemy/src/main.c
+++ udemy/src/main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../../mlx/mlx.h"
+#include "constants.h"
 
-void initializeWindow()
+void	param_init(t_param *param)
 {
-
+	param->x = 3;
+	param->y = 4;
+	param->str[0] = 'a';
+	param->str[1] = 'b';
+	param->str[2] = '\0';
 }
 
-int main()
+int	key_press(int keycode, t_param *param)
 {
-	initializeWindow();
-	return (0);
+	if (keycode == KEY_ESC)
+		exit(0);
+	return (TRUE);
+}
+
+int		main()
+{
+	void *mlx;
+	void *win;
+	t_param	param;
+
+	param_init(&param);
+	if (!(mlx = mlx_init()))
+		return (FALSE);
+	if (!(win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d_window")))
+		return (FALSE);
+	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_loop(mlx);
+	//mlx_destroy_window(mlx, win);
+	return (TRUE);
 }
