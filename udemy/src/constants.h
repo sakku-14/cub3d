5b4ba02c5ae4c2dbf083c50f093ba7400ead20de
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-01-21 15:32:02 +0900

    [Improve] make show white box

--- udemy/src/constants.h
+++ udemy/src/constants.h
@@ -4,15 +4,37 @@
 #define WIN_WIDTH	800
 #define WIN_HEIGHT	600
 
+#define IMG_WIDTH	50
+#define IMG_HEIGHT	50
+
 #define X_EVENT_KEY_PRESS	2
 #define X_EVENT_KEY_release	3
 #define X_EVENT_KEY_EXIT	17
 
 #define KEY_ESC	53
+#define KEY_LEFT	123
+#define KEY_RIGHT	124
+#define KEY_UP		126
+#define KEY_DOWN	125
 
-typedef struct	s_param {
+typedef struct	s_param
+{
 	int		x;
 	int		y;
 	char	str[3];
 }				t_param;
 
+typedef struct	s_mlx
+{
+	void *mlx_ptr;
+	void *win;
+}				t_mlx;
+
+typedef struct	s_img
+{
+	void	*img_ptr;
+	int		*data;
+	int		size_l;
+	int		bpp;
+	int		endian;
+}				t_img;
--- udemy/src/main.c
+++ udemy/src/main.c
@@ -16,34 +16,46 @@ int	key_press(int keycode, t_param *param)
 {
 	if (keycode == KEY_ESC)
 		exit(0);
+	else if (keycode == KEY_LEFT)
+		param->x--;
+	else if (keycode == KEY_RIGHT)
+		param->x++;
+	else if (keycode == KEY_UP)
+		param->y++;
+	else if (keycode == KEY_DOWN)
+		param->y--;
 	return (TRUE);
 }
-
-/*
-int	setup(void *mlx, void *win, t_param *param)
-{
-	param_init(param);
-	if (!(mlx = mlx_init()))
-		return (FALSE);
-	if (!(win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d_window")))
-		return (FALSE);
-	return (TRUE);
-}
-*/
 
 int		main()
 {
-	void *mlx;
-	void *win;
+	t_mlx	*mlx;
 	t_param	param;
+	t_img	img;
 
 	param_init(&param);
-	if (!(mlx = mlx_init()))
+	if (!(mlx->mlx_ptr = mlx_init()))
 		return (FALSE);
-	if (!(win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d_window")))
+	if (!(mlx->win = mlx_new_window(mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "cub3d_window")))
 		return (FALSE);
-	mlx_hook(win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
-	mlx_loop(mlx);
+
+	img.img_ptr = mlx_new_image(mlx->mlx_ptr, IMG_WIDTH, IMG_HEIGHT);
+	img.data = (int *)mlx_get_data_addr(img.img_ptr, &img.bpp, &img.size_l, &img.endian);
+
+	int x = -1;
+	int y = -1;
+	while (++y < IMG_HEIGHT)
+	{
+		x = -1;
+		while (++x < IMG_WIDTH)
+		{
+			img.data[y * IMG_WIDTH + x] = 0xFFFFFF;
+		}
+	}
+
+	mlx_hook(mlx->win, X_EVENT_KEY_PRESS, 0, &key_press, &param);
+	mlx_put_image_to_window(mlx->mlx_ptr, mlx->win, img.img_ptr, param.x, param.y);
+	mlx_loop(mlx->mlx_ptr);
 	//mlx_destroy_window(mlx, win);
 	return (TRUE);
 }
