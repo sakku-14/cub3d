c93b78053f1155d851191c9141c52b49bd075d79
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-03-08 21:22:08 +0900

    [Improve] change some varticals

--- udemy/srcs/first_conf.c
+++ udemy/srcs/first_conf.c
@@ -6,7 +6,7 @@
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 19:07:20 by ysakuma           #+#    #+#             */
-/*   Updated: 2021/03/05 19:07:23 by ysakuma          ###   ########.fr       */
+/*   Updated: 2021/03/08 21:19:21 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void
 	cub_conf(t_cub *cub)
 {
-	cub->conf.pl_x = (cub->conf.pl_x + 0.5) * TILE_SIZE;
-	cub->conf.pl_y = (cub->conf.pl_y + 0.5) * TILE_SIZE;
+	cub->conf.pl_x = (cub->conf.pl_x + 0.501) * TILE_SIZE;
+	cub->conf.pl_y = (cub->conf.pl_y + 0.501) * TILE_SIZE;
 	cub->player.width = 4;
 	cub->player.height = 4;
 	cub->player.walk_speed = 3;
