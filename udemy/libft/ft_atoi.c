2a2c8aaaa62acd2c14b04e7ee07dc2b4ddd0381f
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-03-10 11:03:02 +0900

    [Improve] ft_atoi for over llong

--- udemy/libft/ft_atoi.c
+++ udemy/libft/ft_atoi.c
@@ -5,35 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ysakuma <ysakuma@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/13 22:47:01 by ysakuma           #+#    #+#             */
-/*   Updated: 2021/03/09 18:26:17 by ysakuma          ###   ########.fr       */
+/*   Created: 2021/03/10 11:00:27 by ysakuma           #+#    #+#             */
+/*   Updated: 2021/03/10 11:00:43 by ysakuma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+static int	over_check(long r, char c)
 {
-	long long int	num;
-	int				flag;
+	if (r > LONG_MAX / 10 || (r == LONG_MAX / 10 && c - '0' > LONG_MAX % 10))
+		return (-1);
+	if (r < LONG_MIN / 10 || \
+		(r == LONG_MIN / 10 && c - '0' > LONG_MIN % 10 * -1))
+		return (0);
+	return (1);
+}
 
-	num = 0;
-	flag = 0;
-	while (('\t' <= *str && *str <= '\r') || *str == ' ')
-		str++;
-	if (*str == '+' || *str == '-')
-		if (*str++ == '-')
-			flag = 1;
-	while ('0' <= *str && *str <= '9')
+int			ft_atoi(const char *nptr)
+{
+	long	result;
+	int		p_or_m;
+
+	result = 0;
+	p_or_m = 1;
+	while (((*nptr >= '\t') && (*nptr <= '\r')) || (*nptr == ' '))
+		nptr++;
+	if (*nptr == '-')
 	{
-		num = num * 10 + (*str - '0');
-		str++;
+		p_or_m = -1;
+		nptr++;
 	}
-	if (flag == 1 && -num < LONG_MIN)
-		return ((int)LONG_MIN);
-	if (flag == 1)
-		return (-((int)num));
-	if (num > LONG_MAX)
-		return ((int)LONG_MAX);
-	return ((int)num);
+	else if (*nptr == '+')
+		nptr++;
+	while (over_check(result, *nptr) == 1 && ((*nptr >= '0') && (*nptr <= '9')))
+	{
+		result *= 10;
+		if (p_or_m == 1)
+			result += *nptr++ - '0';
+		else
+			result -= *nptr++ - '0';
+	}
+	if (over_check(result, *nptr) != 1 && ((*nptr >= '0') && (*nptr <= '9')))
+		return (over_check(result, *nptr));
+	return ((int)result);
 }
